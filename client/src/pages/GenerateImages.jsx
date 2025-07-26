caa631ce25290222e6b70321c4480a55f75ba23a
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2025-07-26 17:17:47 +0530

    Add Generate img

--- client/src/pages/GenerateImages.jsx
+++ client/src/pages/GenerateImages.jsx
@@ -1,9 +1,106 @@
-import React from "react";
+import { Image, Sparkles } from "lucide-react";
+import React, { useState } from "react";
 
 const GenerateImages = () => {
+  const imageStyle = [
+    "Realistic",
+    "Ghibli style",
+    "Anime style",
+    "cartoon style",
+    "Fantasy style",
+    "Realistic style",
+    "3 D style",
+    "Portrait style",
+  ];
+
+  const [selectedStyle, setSelectedStyle] = useState("Realistic");
+  const [input, setInput] = useState("");
+  const [publish, setPublish] = useState(false);
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>GenerateImages</h1>
+    <div className="min-h-screen w-full bg-[#F5F9FF] p-6 flex flex-col md:flex-row gap-4 items-start text-slate-700 font-sans">
+      {/* Left Column - Article Configuration */}
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full md:w-1/2 p-6 bg-white rounded-lg border border-gray-200 shadow-sm"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#00AD25]" />
+          <h1 className="text-xl font-semibold">AI Image Generator</h1>
+        </div>
+
+        <p className="mt-6 text-sm font-medium">Describe Your Image</p>
+        <textarea
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+          rows={4}
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
+          placeholder="Describe What you want to see in the image..."
+          required
+        />
+
+        <p className="mt-4 text-sm font-medium">Style</p>
+        <div className="mt-3 flex gap-3 flex-wrap">
+          {imageStyle.map((item) => (
+            <span
+              onClick={() => setSelectedStyle(item)}
+              className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${
+                selectedStyle === item
+                  ? "bg-green-50 text-green-700 border-blue-200"
+                  : "text-gray-500 border-gray-300"
+              }`}
+              key={item}
+            >
+              {item}
+            </span>
+          ))}
+        </div>
+        <div className="my-6 flex items-center gap-2">
+          <label className="cursor-pointer">
+            <input
+              type="checkbox"
+              onChange={(e) => setPublish(e.target.checked)}
+              checked={publish}
+              className="sr-only peer"
+            />
+            <div className="relative w-10 h-6 bg-slate-300 rounded-full peer-checked:bg-green-500 transition-colors">
+              <div
+                className={`absolute top-0.5 left-0.5 w-5 h-5 bg-white rounded-full shadow-md transform transition-transform duration-300 ${
+                  publish ? "translate-x-4" : ""
+                }`}
+              ></div>
+            </div>
+          </label>
+          <p className="text-sm">Make this image Public</p>
+        </div>
+
+        <button
+          type="submit"
+          className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00AD25] to-[#04FF50] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer"
+        >
+          <Image className="w-5" />
+          Generate Image
+        </button>
+      </form>
+
+      {/* Right Column - Generated Article */}
+      <div className="w-full md:w-1/2 p-6 bg-white rounded-lg border border-gray-200 shadow-sm min-h-[405px] flex flex-col">
+        <div className="flex items-center gap-3 mb-4">
+          <Image className="w-5 h-5 text-[#00AD25]" />
+          <h1 className="text-xl font-semibold">Generated image</h1>
+        </div>
+
+        <div className="flex-1 flex justify-center items-center text-gray-400 text-sm text-center px-4">
+          <div className="flex flex-col items-center gap-3">
+            <Image className="w-9 h-9" />
+            <p>Enter a topic and click "Generate image" to get started</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
